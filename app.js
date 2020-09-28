a161a7796f60c029f5b4fe29ade15724cb8f6e89
ellgenius <hagras10@gmail.com>
2020-09-28 02:15:50 +0200

    Create show and hide (results and loadingImg) functions

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // Listen for submit
-document.getElementById('requirementsForm').addEventListener('submit', calculateFinalResults);
+document.getElementById('requirementsForm').addEventListener('submit', function(e){
+  e.preventDefault();
 
-// Create calculateFinalResults function
-function calculateFinalResults(e) {
-  // Assign UI variables
+  // Hide results
+  hideResults();
+
+  // Show loadingImg
+  showLoadingImg();
+  
+  // Run calculateFinalResults function after two seconds
+  setTimeout(calculateFinalResults, 2000);
+});
+
+// Assign UI variables
   // Requirements variables
   const inputLoanAmount = document.querySelector('.inputLoanAmount');
   const inputInterest = document.querySelector('.inputInterest');
@@ -13,26 +22,39 @@ function calculateFinalResults(e) {
   const inputTotalPayment = document.querySelector('.inputTotalPayment');
   const inputTotalInterest = document.querySelector('.inputTotalInterest');
 
+// Create calculateFinalResults function
+function calculateFinalResults(loadingImg,results) {
 
-  const totalInterest = parseFloat(((inputLoanAmount.value * inputInterest.value) / 100) * inputYearsToRepay.value);
   if (inputLoanAmount.value === "" || inputInterest.value === "" || inputYearsToRepay.value === "") {
-    //Create div for warning
+    // Hide results and loadingImg
+    hideLoadingImg();
+    hideResults();
+    //Create errorDiv for warning
     const errorDiv = document.createElement('div');
     errorDiv.className = 'alert alert-danger';
-    errorDiv.appendChild(document.createTextNode('Empty values'));
+    errorDiv.appendChild(document.createTextNode('An empty field, so Kindly recheck your entry'));
     // Insert the error above the heading
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
     card.insertBefore(errorDiv, heading);
-    // Clear result fields
+    // Clear Result Fields
     clearResultFields();
-
-
+    // Remove ErrorDiv
+    removeErrorDiv(errorDiv);
   } else {
-    inputTotalInterest.value = totalInterest.toFixed(2);
+    // calculate total interest
+    const calculateTotalInterest = parseFloat(((inputLoanAmount.value * inputInterest.value) / 100) * inputYearsToRepay.value);
+    inputTotalInterest.value = calculateTotalInterest.toFixed(2);;
+    // calculate Total Payment
+    const calculateTotalPayment = parseFloat(Number(inputLoanAmount.value) + calculateTotalInterest);
+    inputTotalPayment.value = calculateTotalPayment.toFixed(2);;
+    // calculate Monthly Payment
+    const calculateMonthlyPayment = parseFloat(calculateTotalPayment / (Number(inputYearsToRepay.value)*12));
+    inputMonthlyPayment.value = calculateMonthlyPayment.toFixed(2);
+    // Hide loadingImg and show results
+    hideLoadingImg();
+    showResults()
   }
-
-  e.preventDefault();
 }
 
 // Create clearResultFields function
@@ -40,4 +62,28 @@ function clearResultFields() {
   inputMonthlyPayment.value = '';
   inputTotalPayment.value = '';
   inputTotalInterest.value = '';
-};
+}
+
+// Remove errorDiv
+function removeErrorDiv(errorDiv) {
+  setTimeout(() => {
+    errorDiv.remove();
+  }, 3000);
+}
+
+// Show loadingImg function
+function showLoadingImg(){
+  document.querySelector('.loadingImg').setAttribute('style', 'display: block !important');
+}
+// Hide loadingImg function
+function hideLoadingImg(){
+  document.querySelector('.loadingImg').setAttribute('style', 'display: none !important');
+}
+// Show results function
+function showResults(){
+  document.querySelector('.results').setAttribute('style', 'display: block !important');
+}
+// Hide results function
+function hideResults(){
+  document.querySelector('.results').setAttribute('style', 'display: none !important');
+}
